57c49e57fc60d7b719f401d2930b0e359356a378
kazshi01 <blumedio0619@gmail.com>
2025-09-12 06:54:23 +0900

    コメント追加

--- internal/interface/http/order_handler.go
+++ internal/interface/http/order_handler.go
@@ -8,29 +8,40 @@ import (
 	"github.com/kazshi01/payment-system/internal/usecase"
 )
 
-type OrderHandler struct{ UC *usecase.OrderUsecase }
+type OrderHandler struct {
+	UC *usecase.OrderUsecase
+}
 
+// 注文を作成する
 func (h *OrderHandler) Create(w http.ResponseWriter, r *http.Request) {
+
 	var body struct {
 		AmountJPY int64 `json:"amount_jpy"`
 	}
+
 	if err := json.NewDecoder(r.Body).Decode(&body); err != nil {
 		http.Error(w, err.Error(), 400)
 		return
 	}
+
 	o, err := h.UC.CreateOrder(r.Context(), body.AmountJPY)
 	if err != nil {
 		http.Error(w, err.Error(), 500)
 		return
 	}
+
 	_ = json.NewEncoder(w).Encode(o)
 }
 
+// 注文を支払いする
 func (h *OrderHandler) Pay(w http.ResponseWriter, r *http.Request) {
+
 	id := order.ID(r.PathValue("id"))
+
 	if err := h.UC.PayOrder(r.Context(), id); err != nil {
 		http.Error(w, err.Error(), 400)
 		return
 	}
+
 	w.WriteHeader(http.StatusNoContent)
 }
--- internal/usecase/order_usecase.go
+++ internal/usecase/order_usecase.go
@@ -16,7 +16,10 @@ type OrderUsecase struct {
 
 func generateID() string { return "1" } // これは後で実装！！
 
+// 注文を作成する
 func (uc *OrderUsecase) CreateOrder(ctx context.Context, amountJPY int64) (*order.Order, error) {
+
+	// ① 注文を作成
 	o := &order.Order{
 		ID:        order.ID(generateID()), // 実装は省略
 		AmountJPY: amountJPY,
@@ -24,27 +27,41 @@ func (uc *OrderUsecase) CreateOrder(ctx context.Context, amountJPY int64) (*orde
 		CreatedAt: time.Now(),
 		UpdatedAt: time.Now(),
 	}
+
+	// ② 注文をDBに保存
 	if err := uc.Repo.Create(ctx, o); err != nil {
 		return nil, err
 	}
+
 	return o, nil
 }
 
+// 注文を支払いする
 func (uc *OrderUsecase) PayOrder(ctx context.Context, id order.ID) error {
 	return uc.Tx.Do(ctx, func(ctx context.Context) error {
+
+		// ① 注文を取得
 		o, err := uc.Repo.FindByID(ctx, id)
 		if err != nil {
 			return err
 		}
+
+		// ② 外部決済サービスで支払い実行
 		txID, err := uc.PG.Charge(ctx, domain.PaymentIntent{
 			OrderID: string(o.ID), Amount: o.AmountJPY, Currency: "jpy",
 		})
 		if err != nil {
 			return err
 		}
+
+		// ③ 後で保存する予定なので今は未使用
 		_ = txID // 後で payments / events に保存
+
+		// ④ 注文ステータスを「支払い済み」に変更
 		o.MarkPaid()
 		o.UpdatedAt = time.Now()
+
+		// ⑤ 注文情報をDBに更新
 		return uc.Repo.Update(ctx, o)
 	})
 }
